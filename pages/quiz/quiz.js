73fc31087cf31947c0670f6458b2a2e87cd7be74
Eduardo Oliveira <eduardocavallin2005@gmail.com>
2024-10-25 00:34:28 -0300

    Questões corretas e erradas

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -3,7 +3,9 @@ const assunto = localStorage.getItem("assunto")
 let quiz = {}
 let pontos = 0
 let pergunta = 1
-
+let resposta = ""
+let idInputResposta = ""
+let respostaCorretaId = ""
 function alterarAssunto () {
   const divIcone = document.querySelector(".assunto_icone")
   const iconeImg = document.querySelector(".assunto_icone img")
@@ -45,7 +47,7 @@ function montarPergunta() {
         <section class="alternativas">
             <form action="">
               <label for="alternativa_a">
-                <input type="radio" id="alternativa_a" name="alternativa">
+                <input type="radio" id="alternativa_a" name="alternativa" value="${quiz.questions[pergunta-1].options[0]}">
 
                 <div>
                   <span>A</span>
@@ -54,7 +56,7 @@ function montarPergunta() {
               </label>
 
               <label for="alternativa_b">
-                  <input type="radio" id="alternativa_b" name="alternativa">
+                  <input type="radio" id="alternativa_b" name="alternativa" value="${quiz.questions[pergunta-1].options[1]}">
 
                   <div>
                     <span>B</span>
@@ -63,7 +65,7 @@ function montarPergunta() {
               </label>
 
               <label for="alternativa_c">
-                <input type="radio" id="alternativa_c" name="alternativa">
+                <input type="radio" id="alternativa_c" name="alternativa" value="${quiz.questions[pergunta-1].options[2]}">
                 
                 <div>
                   <span>C</span>
@@ -72,7 +74,7 @@ function montarPergunta() {
               </label>
 
               <label for="alternativa_d">
-                <input type="radio" id="alternativa_d" name="alternativa">
+                <input type="radio" id="alternativa_d" name="alternativa" value="${quiz.questions[pergunta-1].options[3]}">
 
                 <div>
                   <span>D</span>
@@ -86,12 +88,36 @@ function montarPergunta() {
   `
 }
 
+function guardarResposta(evento) {
+  resposta = evento.target.value
+  idInputResposta = evento.target.id
 
+  const botaoEnviar = document.querySelector(".alternativas button")
+  botaoEnviar.addEventListener("click", validarResposta)
+}
+
+function validarResposta() {
+  if (resposta === quiz.questions[pergunta-1].answer) {
+    document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "correta")
+    pontos = pontos + 1 
+  }else{
+    document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "errada")
+    document.querySelector(`label[for='${respostaCorretaId}']`).setAttribute("id", "correta")
+  }
+}
 
 async function iniciar() {
   alterarAssunto()
   await buscarPerguntas()
   montarPergunta()
+
+  const inputResposta = document.querySelectorAll(".alternativas input")
+  inputResposta.forEach(input => {
+    input.addEventListener("click", guardarResposta)
+    if (input.value === quiz.questions[pergunta-1].answer){
+      respostaCorretaId = input.id
+    }
+  })
 }
 
 iniciar ()
